099151551b8e6773817e4b346063cfbe7aeca285
Drantiss <54062650+LixiaoLeo123@users.noreply.github.com>
2025-10-31 20:43:16 +0800

    Update Essentials.h

--- Essentials.h
+++ Essentials.h
@@ -1,14 +1,16 @@
 #pragma once
 #ifndef ESSENTIALS_H
 #define ESSENTIALS_H
-template<typename T>
-inline void swap(T& a, T& b) {
-	T temp = a;
-	a = b;
-	b = temp;
-}
-template<typename T>
-inline T abs(const T& x) {
-	return (x < 0) ? (-x) : x;
+namespace drantiss{
+	template<typename T>
+	inline void swap(T& a, T& b) {
+		T temp = a;
+		a = b;
+		b = temp;
+	}
+	template<typename T>
+	inline T abs(const T& x) {
+		return (x < 0) ? (-x) : x;
+	}
 }
 #endif
